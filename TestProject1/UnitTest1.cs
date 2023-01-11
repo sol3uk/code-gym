002fd92d79deac43521e73c77ff0758a8d743efa
Ben Ashley <ben.ashley@asos.com>
2023-01-11 15:36:07 +0000

    Initial commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,124 @@
+using System.Text;
+using NUnit.Framework;
+
+namespace TestProject1;
+
+public class Tests
+{
+    [TestFixture]
+    public class People
+    {
+        [Test]
+        public void GivenAListOfPeopleWhenFilteredThenAllAgesShouldBeGreaterOrEqualTo18()
+        {
+            var employees = new List<Usings.Person>
+            {
+                new("Max", 17),
+                new("Sepp", 18),
+                new("Nina", 15),
+                new("Mike", 51)
+            };
+            var expectedEmployees = new List<Usings.Person>
+            {
+                new("Sepp", 18),
+                new("Mike", 51)
+            };
+
+            var result = Usings.FilterAge(employees);
+
+            Assert.That(result.All(person => person.age >= 18));
+            Assert.That(expectedEmployees.Count, Is.EqualTo(result.Count));
+        }
+
+        [Test]
+        public void GivenAListOfPeopleWhenFilteredThenAllNamesShouldBeInDescendingOrder()
+        {
+            var employees = new List<Usings.Person>
+            {
+                new("Max", 17),
+                new("Sepp", 18),
+                new("Nina", 15),
+                new("Mike", 51)
+            };
+            var expectedEmployees = new List<Usings.Person>
+            {
+                new("SEPP", 18),
+                new("MIKE", 51),
+            };
+
+            var result = Usings.FilterAge(employees);
+
+            Assert.That(expectedEmployees, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void GivenAListOfPeopleWhenFilteredThenAllNamesShouldBeInCaps()
+        {
+            var employees = new List<Usings.Person>
+            {
+                new("Max", 17),
+                new("Sepp", 18),
+                new("Nina", 15),
+                new("Mike", 51)
+            };
+            var expectedEmployees = new List<Usings.Person>
+            {
+                new("MIKE", 51),
+                new("SEPP", 18),
+            };
+
+            var result = Usings.FilterAge(employees);
+
+            Assert.That(expectedEmployees, Is.EquivalentTo(result));
+        }
+    }
+
+    [TestFixture]
+    public class Mumble
+    {
+        [TestCase("abcd", "A-Bb-Ccc-Dddd")]
+        [TestCase("RqaE4zty", "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy")]
+        [TestCase("cw At", "C-Ww-Aaa-Tttt")]
+        [TestCase("", "")]
+        [TestCase("3", "")]
+        public void GivenAnInputWhenMumblingIsCalledThenItReturnsExpectedOutput(string testInput, string expectedOutput)
+        {
+            var result = Usings.Mumbling(testInput);
+
+            Assert.That(result, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void GivenAnInputWhenMumblingIsCalledThenTheResultContainsHyphens()
+        {
+            var testInput = "abcd";
+
+            var result = Usings.Mumbling(testInput);
+
+            Assert.That(result.Contains("-"), Is.True);
+        }
+
+        [Test]
+        public void GivenAnInputWhenMumblingIsCalledThenTheResultContainsCapitalisedLetters()
+        {
+            var testInput = "abcd";
+
+            var result = Usings.Mumbling(testInput);
+
+            Assert.That(result.Contains("A"), Is.True);
+            Assert.That(result.Contains("B"), Is.True);
+            Assert.That(result.Contains("C"), Is.True);
+            Assert.That(result.Contains("D"), Is.True);
+        }
+
+        [Test]
+        public void GivenAnInputWhenMumblingIsCalledThenTheResultContainsDuplicatesOfEachLetter()
+        {
+            var testInput = "abcd";
+
+            var result = Usings.Mumbling(testInput);
+
+            Assert.That(result, Is.EqualTo("A-Bb-Ccc-Dddd"));
+        }
+    }
+}
---
+++ TestProject1/Usings.cs
@@ -0,0 +1,36 @@
+namespace TestProject1;
+
+public class Usings
+{
+    public static string Mumbling(string input)
+    {
+        var charArray = input.ToLower()
+            .Replace(" ", "")
+            .Where(x => !char.IsNumber(x)).Select(x => x)
+            .ToArray();
+        var result = new List<string>();
+        for (var index = 0; index < charArray.Length; index++)
+        {
+            var t = charArray[index];
+            var duplicates = new string(t, index);
+            result.Add(t.ToString().ToUpper() + duplicates);
+        }
+
+        return string.Join("-", result);
+    }
+
+    #region Person
+
+    public record Person(string name, int age);
+
+    public static List<Person> FilterAge(List<Person> employees)
+    {
+        return employees
+            .Where(person => person.age >= 18)
+            .OrderByDescending(person => person.name)
+            .Select(person => person with {name = person.name.ToUpperInvariant()})
+            .ToList();
+    }
+
+    #endregion
+}
